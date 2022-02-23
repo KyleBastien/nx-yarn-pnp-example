6526fbb089e9bdfdf2f7b58db87f1e965d63dde2
Kyle.Bastien <Kyle.Bastien@docusign.com>
2022-02-23 12:13:33 -0800

    Removing tslib call from react app to get unit tests working

--- packages/react-app/src/app/app.tsx
+++ packages/react-app/src/app/app.tsx
@@ -1,10 +1,7 @@
 import { ReactLib } from '@nx-yarn-pnp-example/react-lib';
-import { tsLib } from '@nx-yarn-pnp-example/ts-lib';
 import NxWelcome from './nx-welcome';
 
 export function App() {
-  tsLib();
-
   return (
     <>
       <NxWelcome title="react-app" />
