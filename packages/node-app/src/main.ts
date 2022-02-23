8e4f651b51057073eb7f73c4a3e528177ea54adf
Kyle Bastien <mcnuttkyle93@gmail.com>
2022-02-23 12:25:15 -0800

    Adding initial examples using npm (#1)
    * Initial Nx commit
    
    * Adding Angular app example + angular lib example
    
    * Adding React app + react-lib example
    
    * Adding node-app + node-lib example
    
    * Adding ts lib example + usage
    
    * Adding github actions workflow
    
    * Changing tslib to not have export syntax
    
    * Revert "Changing tslib to not have export syntax"
    
    This reverts commit 52320919735594aae81359bc1199c50f885e79cf.
    
    * Removing tslib call from react app to get unit tests working
    
    * Fixing e2e parallel to not block ports
    
    * Really this time fix e2e
    
    Co-authored-by: Kyle.Bastien <Kyle.Bastien@docusign.com>

---
+++ packages/node-app/src/main.ts
@@ -0,0 +1,8 @@
+import { nodeLib } from '@nx-yarn-pnp-example/node-lib';
+import { tsLib } from '@nx-yarn-pnp-example/ts-lib';
+
+console.log('Hello World!');
+
+console.log(nodeLib());
+
+console.log(tsLib());
