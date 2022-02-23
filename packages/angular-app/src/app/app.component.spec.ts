85e539b1d3ecba1f54d50aaa8310b631076ffc76
Kyle.Bastien <Kyle.Bastien@docusign.com>
2022-02-23 12:03:12 -0800

    Revert "Changing tslib to not have export syntax"
    This reverts commit 52320919735594aae81359bc1199c50f885e79cf.

--- packages/angular-app/src/app/app.component.spec.ts
+++ packages/angular-app/src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { AngularLibModule } from '@nx-yarn-pnp-example/angular-lib';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AngularLibModule],
       declarations: [AppComponent, NxWelcomeComponent],
     }).compileComponents();
   });
