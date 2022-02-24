e96c67ea533f2732d7623ee70c4116d6a936bd89
Kyle.Bastien <Kyle.Bastien@docusign.com>
2022-02-24 11:35:09 -0800

    Fix angular-app + add vscode sdks

--- packages/angular-app/src/app/app.component.ts
+++ packages/angular-app/src/app/app.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { tsLib } from '@nx-yarn-pnp-example/ts-lib';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'nx-yarn-pnp-example-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular-app';
-
-  ngOnInit(): void {
-    tsLib();
-  }
 }
